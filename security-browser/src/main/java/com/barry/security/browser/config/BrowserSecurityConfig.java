d9d06e1b1beb73b7f913e483f6b9b9430d196076
hts <htbyanghang2@163.com>
2019-11-04 11:22:34 +0800

    init commit

--- security-browser/src/main/java/com/barry/security/browser/config/BrowserSecurityConfig.java
+++ security-browser/src/main/java/com/barry/security/browser/config/BrowserSecurityConfig.java
@@ -1,18 +1,36 @@
 package com.barry.security.browser.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
+        http
+                .csrf()
+                .disable()
+                .formLogin()
+                .loginPage("/signIn.html")
+                .loginProcessingUrl("/authentication/form")
                 .and()
                 .authorizeRequests()
+                .antMatchers("/signIn.html").permitAll()
                 .anyRequest()
                 .authenticated();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
---
+++ security-browser/src/main/java/com/barry/security/browser/service/MyUserDetailService.java
@@ -0,0 +1,29 @@
+package com.barry.security.browser.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyUserDetailService implements UserDetailsService {
+
+    private static final Logger log = LoggerFactory.getLogger(MyUserDetailService.class);
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String password = passwordEncoder.encode("123456");
+        log.info("==MyUserDetailService=====>username:{}, password:{}", username, password);
+        User user = new User(username, password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        return user;
+    }
+}
